fc0e862c5f5a8ea5e0691ad15c643899544e839d
selimcan-stack <selimcan@a2adijital.com>
2026-04-20 20:44:00 +0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,17 @@
-const CACHE_NAME = 'myswan-v1';
+const CACHE_NAME = 'myswan-v2';
 
 self.addEventListener('install', (event) => {
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(clients.claim());
+  event.waitUntil(
+    caches.keys().then((names) => {
+      return Promise.all(
+        names.map((name) => caches.delete(name))
+      );
+    }).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', (event) => {
